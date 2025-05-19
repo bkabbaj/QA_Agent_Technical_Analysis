08f24220b43c400b28dd39934770c6d79787e7dc
bkabbaj <badr.kabbaj1@gmail.com>
2025-05-19 12:57:18 +0200

    Update app.py port

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def whatsapp():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render will set PORT env var
+    app.run(host="0.0.0.0", port=port)
